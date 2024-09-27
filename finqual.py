248b6188b648dc44d47c99246363ba30beff05c0
harry <harryhe99@outlook.com>
2024-09-28 00:59:47 +0100

    Fixed for company_tickers.json

--- finqual.py
+++ finqual.py
@@ -1,4 +1,5 @@
 # Import packages
+import json
 import pandas as pd
 import numpy as np
 import requests
@@ -70,14 +71,24 @@ class Ticker():
         self.SIC = self.SIC()
 
     def CIK(self):
-        headers = {"Accept": "application/json, text/plain, */*", "Accept-Encoding": "gzip, deflate, br",
-                   "Accept-Language": "en-US,en;q=0.9",
-                   "Origin": "https://www.nasdaq.com",
-                   "Referer": "https://www.nasdaq.com",
-                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}
 
-        source = requests.get(url="https://www.sec.gov/files/company_tickers.json", headers=headers, verify=True)
-        cik = source.json()
+        try:
+            headers = {"Accept": "application/json, text/plain, */*",
+                       "Accept-Language": "en-US,en;q=0.9",
+                       "Origin": "https://www.nasdaq.com",
+                       "Referer": "https://www.nasdaq.com",
+                       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}
+
+            source = requests.get(url="https://www.sec.gov/files/company_tickers.json", headers=headers, verify=True)
+            cik = source.json()
+
+        except:
+            this_dir, this_filename = os.path.split(__file__)
+            source = os.path.join(this_dir, "data", "company_tickers.json")
+
+            with open(source) as f:
+                cik = json.load(f)
+
         cik = pd.DataFrame(cik)
         cik = cik.transpose()
 
@@ -164,9 +175,17 @@ class Ticker():
                    "Referer": "https://www.nasdaq.com",
                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}
 
-        # Getting the CIK number of chosen company
-        source = requests.get(url="https://www.sec.gov/files/company_tickers.json", headers=headers, verify=True)
-        cik = source.json()
+        try:
+            source = requests.get(url="https://www.sec.gov/files/company_tickers.json", headers=headers, verify=True)
+            cik = source.json()
+
+        except:
+            this_dir, this_filename = os.path.split(__file__)
+            source = os.path.join(this_dir, "data", "company_tickers.json")
+
+            with open(source) as f:
+                cik = json.load(f)
+
         cik = pd.DataFrame(cik)
         cik = cik.transpose()
 
@@ -393,7 +412,6 @@ class Ticker():
         nodes = [s_b,s_d]
 
         data = [self.year_tree_item(i, start, end, category = "EPS", quarter = True) for i in nodes]
-        print(no_columns)
 
         for i in range(len(data)):
             if no_columns == -1:
