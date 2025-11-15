9cc3d3032333368549f8dbffe5573793b266f87b
harry <harryhe99@outlook.com>
2025-11-15 23:29:03 +0000

    v4.5.8 - Added entities

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="finqual",  # This must be unique on PyPI
-    version="4.5.7",
+    version="4.5.8",
     author="Harry",
     description="A Python package to help investors to conduct financial research, analysis and comparable company analysis.",
     long_description=open("README.md", encoding="utf-8").read(),
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/harryy-he/finqual",  # optional, update if you have a repo
     packages=find_packages(),  # Automatically includes finqual/
     include_package_data=True,
-    package_data={'finqual': ['data/*.parquet', 'data/*.json', 'node_classes/*.py', 'sec_edgar/*.py']},
+    package_data={'finqual': ['data/*.parquet', 'data/*.json', 'node_classes/*.py', 'sec_edgar/*.py', 'sec_edgar/entities/*.py']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # or whichever license you use
