66ced6b558cbdc31acb643e41112d09318bcc216
GL-HH <harry.he@gamalife.com>
2025-12-01 15:46:07 +0000

    added uuid

--- finqual/config/headers.py
+++ finqual/config/headers.py
@@ -1,8 +1,10 @@
+import uuid
+
 sec_headers = {
     "Accept": "application/json, text/plain, */*",
     "Accept-Language": "en-US,en;q=0.9",
     "Origin": "https://www.nasdaq.com",
     "Referer": "https://www.nasdaq.com",
-    "User-Agent": 'YourName (your@email.com)', #TODO: add UUID
+    "User-Agent": f"Your Name (your@email.com; {uuid.uuid4()})",
     'Accept-Encoding': 'gzip, deflate',
 }
